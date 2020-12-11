b3cedf44efc6b1ccd82ba460f1b40eb39a367189
German Middi <german@onmedia.com.ar>
2020-12-11 19:08:15 -0300

    ajustes

--- src/resources/js/components/posts/PostIndexComponent.vue
+++ src/resources/js/components/posts/PostIndexComponent.vue
@@ -3,16 +3,41 @@
     <div class="card">
         <div class="card-header">
             Articulos
-            <a href="" class="btn btn-sm btn-primary float-right">Crear</a>
+            <a @click="goToCreatePost" class="btn btn-sm btn-primary float-right">Crear</a>
         </div>
 
         <div class="card-body">
-            <b-table hover :sort-by.sync="sortBy" :items="items" :fields="fields" :key="items.id">
+            <b-table 
+            hover 
+            :sort-by.sync="sortBy"
+            :sort-desc.sync="sortDesc"
+            :sort-direction="sortDirection"
+            :items="items" 
+            :fields="fields" 
+            :key="items.id"       
+            :current-page="currentPage"
+            :per-page="perPage"
+            >
+
                 <template #cell(actions)="row">
                     <b-button pill variant="outline-primary" class="mr-2" ><b-icon-pencil></b-icon-pencil></b-button>
                     <b-button pill variant="outline-danger" class="mr-2" ><b-icon-trash></b-icon-trash></b-button>
                 </template>
             </b-table>
+
+            <div class="row">
+                <div class="col-4">
+                    <b-pagination
+                    v-model="currentPage"
+                    :total-rows="totalRows"
+                    :per-page="perPage"
+                    align="fill"
+                    class="my-0"
+                    >
+                    </b-pagination>
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -24,19 +49,29 @@
             return {
                 items: [],
                 fields: [
-                        { key: 'id',    label: 'ID', sortDirection: 'desc' },
+                        { key: 'id',    label: 'ID', sortable: true, sortDirection: 'desc' },
                         { key: 'title', label: 'Título'},
-                        { key: 'created_at', label: 'Creado'},
+                        { key: 'created_at', label: 'Creado', sortable: true},
                         { key: 'actions', label: 'Actions' }
-                ]
+                ],
+                currentPage: 1,
+                perPage: 10,
+                totalRows: 1,
+                sortBy: 'created_at',
+                sortDesc: true,
+                sortDirection: 'desc',                 
             }
         },
-        // methods:{
-        // },
+        methods:{
+            goToCreatePost(){
+                window.location = '/posts/create'
+            }
+        },
         mounted(){
             axios.get('/posteos')
             .then((response) => {
                 this.items = response.data.data
+                this.totalRows = this.items.length
             })
             console.log('Component mounted.')            
         }    
